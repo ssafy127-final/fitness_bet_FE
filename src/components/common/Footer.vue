eaaa58460062dd0b3adf2e29d1c5630e3a816f41
sohyun <ksh96611@naver.com>
2024-11-15 18:48:23 +0900

    Feat: footer 설정

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>
-        <h2>Footer.vue</h2>
-    </div>
+  <div class="container">
+    <h4>© 2024. Teamname. All rights reserved.</h4>
+  </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped>
-
+.container {
+  height: 30px;
+  background-color: #607180;
+  text-align: center;
+  color: white;
+}
+h4 {
+  line-height: 30px;
+}
 </style>
