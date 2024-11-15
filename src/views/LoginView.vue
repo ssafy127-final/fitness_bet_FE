ec46fade5c51ad380d98a0eac4a9f65546fed4c5
Huk9uri <huk9uri@gmail.com>
2024-11-15 16:41:51 +0900

    feat : Mission Component vue 생성 및 index.js에 등록, header 설정

---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>로그인페이지</h2>
+        <LoginForm></LoginForm>
+    </div>
+</template>
+
+<script setup>
+import LoginForm from '@/components/login/LoginForm.vue';
+
+</script>
+
+<style scoped>
+
+</style>
