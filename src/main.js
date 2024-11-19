8645da6f690fb2f9e19dc277640469bcaa540bf5
Huk9uri <huk9uri@gmail.com>
2024-11-19 17:44:11 +0900

    fix : session 사용을 위한 main.js의 axios.defaults.withCredentials = true; 설정 추가

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import '@/assets/css/style.css';
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import axios from 'axios';
 
 import App from "./App.vue";
 import router from "./router";
+
+axios.defaults.withCredentials = true;
 
 const app = createApp(App);
 
