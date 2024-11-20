079d5587354aa0ac79ce897a59ad87529ea5cd6e
sohyun <ksh96611@naver.com>
2024-11-20 20:16:37 +0900

    feat: pointStore vue 화면 제작

---
+++ src/components/point/StoreItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="store-item">
+    <img :src="props.item.img" :alt="props.item.name" />
+    <h3>{{ props.item.name }}</h3>
+    <p>{{ props.item.point }} Point</p>
+    <button class="change-btn" @click="change">교환하기</button>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  item: Object,
+});
+
+const change = () => {
+  if (confirm("교환하시겠습니까?")) {
+  }
+};
+</script>
+
+<style scoped>
+.store-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3px;
+}
+img {
+  width: 9rem;
+}
+p {
+  font-weight: 600;
+  -webkit-text-stroke: 1.2px rgb(83, 5, 5);
+}
+.change-btn {
+  background-color: yellow;
+  color: black;
+  padding: 5px 10px;
+  font-weight: 600;
+}
+.change-btn:hover {
+  background-color: black;
+  color: white;
+}
+</style>
--- src/views/PointStoreView.vue
+++ src/views/PointStoreView.vue
@@ -1,13 +1,113 @@
 <template>
-    <div>
-        <h1>포인트 스토어</h1>
+  <div class="container">
+    <div class="side" :class="{ change: counter }">
+      <p>포</p>
+      <p>인</p>
+      <p>트</p>
     </div>
+    <div class="content">
+      <StoreItem v-for="item in storeItem" :item="item" :key="item.id" />
+    </div>
+    <div class="side" :class="{ change: counter }">
+      <p>교</p>
+      <p>환</p>
+      <p>소</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import StoreItem from "@/components/point/StoreItem.vue";
+import { onMounted, onUnmounted, ref } from "vue";
 
+const counter = ref(false);
+let intervalId = null;
+
+onMounted(() => {
+  getItemList();
+  intervalId = setInterval(() => {
+    counter.value = !counter.value;
+  }, 100);
+});
+
+const storeItem = ref([]);
+const getItemList = () => {
+  // axios
+  let id = 0;
+  storeItem.value = [
+    {
+      id: id++,
+      name: "바나 아아",
+      price: 1800,
+      img: "https://www.banapresso.com/from_open_storage?ws=fprocess&file=banapresso/menu/img_aa618eecef95.png",
+      delYn: 0,
+      point: 1000,
+    },
+    {
+      id: id++,
+      name: "바나 라떼",
+      price: 2300,
+      img: "https://www.banapresso.com/from_open_storage?ws=fprocess&file=banapresso/menu/img_0a2935abe540.png",
+      delYn: 0,
+      point: 2000,
+    },
+    {
+      id: id++,
+      name: "바나 초코쉐이크",
+      price: 4500,
+      img: "https://www.banapresso.com/from_open_storage?ws=fprocess&file=banapresso/menu/img_0f4de72e3327.png",
+      delYn: 0,
+      point: 4000,
+    },
+  ];
+};
+onUnmounted(() => {
+  clearInterval(intervalId);
+});
 </script>
 
 <style scoped>
-
+.container {
+  background-image: url("/src/assets/img/store_background_color.jpg");
+  /* background-image: url("/src/assets/img/store_background_black.jpg"); */
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-position-y: -40px;
+  height: calc(100vh - 130px);
+  position: relative;
+  isolation: isolate;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+}
+.side {
+  display: flex;
+  flex-direction: column;
+  gap: 5.5rem;
+  font-weight: 800;
+  font-size: 3rem;
+  color: red;
+}
+.change {
+  color: black;
+}
+.container::after {
+  content: "";
+  position: absolute;
+  background: white;
+  z-index: -1;
+  inset: 0;
+  opacity: 0.7;
+}
+.content {
+  height: 100%;
+  width: 70%;
+  padding: 2.5rem 4rem;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: start;
+  gap: 1.5rem;
+}
 </style>
